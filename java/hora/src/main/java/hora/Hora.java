e0d01fa9e0fdf337d8372931df3acc83b98c3f65
Gean Luiz <geanluiz1@gmail.com>
2025-02-15 20:46:05 -0300

    Adds package 'hora'

---
+++ java/hora/src/main/java/hora/Hora.java
@@ -0,0 +1,11 @@
+package hora;
+
+import java.util.Date;
+
+public class Hora {
+
+    public static void main (String[] args) {
+        Date currentDate = new Date();
+        System.out.println(currentDate);
+    }
+}
