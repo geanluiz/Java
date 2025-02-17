f1bb91a44f46883af0bb0460db6ed628375700a4
Gean Luiz S Ribeiro <geanluiz1@gmail.com>
2025-02-17 14:25:51 -0300

    Fix merge conflicts

---
+++ Projects/src/main/java/myproject/Projects.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package horadosistema;
+
+/**
+ *
+ * @author PC TRABALHO
+ */
+public class Projects {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World dever!");
+    }
+}
