ea7f9c1c7f005a936f5a0f1bf8490fe2c939e1b7
Gean Luiz <geanluiz1@gmail.com>
2025-02-18 21:17:01 -0300

    Adds input exercise 'name'

---
+++ name/Name.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Name {
+
+    public static void main(String[] args) {
+
+        Scanner newScan = new Scanner(System.in);
+        System.out.print("Insert your name: ");
+
+        String name = newScan.nextLine();
+        System.out.println("Hello " + name + "!");
+    }
+}
