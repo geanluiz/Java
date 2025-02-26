a8a1c9b5bb1ab989c66fe205976902211d0843bb
Gean Luiz S Ribeiro <geanluiz1@gmail.com>
2025-02-26 14:58:00 -0300

    Added interfaces exercise

---
+++ interfaces/Fish.java
@@ -0,0 +1,12 @@
+public class Fish implements Prey, Predator{
+
+    @Override
+    public void flee() {
+        System.out.println("Fish is fleeing from a bigger fish");
+    }
+
+    @Override
+    public void hunt() {
+        System.out.println("Fish is hunting a smaller fish");
+    }
+}
---
+++ interfaces/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        Hawk hawk = new Hawk();
+        Rabbit rabbit = new Rabbit();
+        Fish smallFish = new Fish();
+        Fish bigFish = new Fish();
+
+        hawk.hunt();
+        rabbit.flee();
+        smallFish.flee();
+        bigFish.hunt();
+    }
+}
