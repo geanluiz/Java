2f601704a1fd3c18d3560d51d8c28c5c5c5ea878
Gean Luiz <geanluiz1@gmail.com>
2025-02-26 21:36:50 -0300

    Added encapsulation exercise

---
+++ encapsulation/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String [] args) {
+
+        Person newPerson = new Person();
+
+        newPerson.setName("John");
+        System.out.println(newPerson.getName());
+    }
+}
---
+++ encapsulation/Person.java
@@ -0,0 +1,14 @@
+public class Person {
+
+    private String name;
+
+
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+}
